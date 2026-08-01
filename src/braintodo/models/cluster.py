04edf6d0653b48b541a6ae371bdb179458c3af63
Kiet Tran Anh <trananhkiet06072005@gmail.com>
2026-08-01 12:07:51 +0700

    add models/cluster

---
+++ src/braintodo/models/cluster.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+
+class Cluster(BaseModel): 
+    """A detected community: a group of nodes that are more densely
+    connected to each other than to the rest of the graph."""
+
+    cluster_id: int 
+    node_ids: list[str]
